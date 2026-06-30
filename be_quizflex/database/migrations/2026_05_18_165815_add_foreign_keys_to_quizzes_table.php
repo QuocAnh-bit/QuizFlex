28136203c97a0055b8520b21255dbc73362c96e1
NQH118 <ngoquanghuy118@gmail.com>
2026-06-30 20:47:08 +0700

    sửa lại bảng xếp hạng

--- be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_quizzes_table.php
+++ be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_quizzes_table.php
@@ -9,22 +9,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('quizzes', function (Blueprint $table) {
-<<<<<<<< HEAD:be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_quizzes_table.php
-            $table->foreign(['user_id'], 'fk_quizzes_user_id')->references(['id'])->on('users')->onUpdate('cascade')->onDelete('cascade');
-========
+            $table->foreign(['user_id'], 'fk_quizzes_user_id')
+                ->references(['id'])
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
             $table->boolean('is_ai_generated')->default(false)->after('user_id');
->>>>>>>> origin/huydev:be_quizflex/database/migrations/2026_05_31_060842_add_columns_to_quizzes_table.php
         });
     }
 
     public function down(): void
     {
         Schema::table('quizzes', function (Blueprint $table) {
-<<<<<<<< HEAD:be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_quizzes_table.php
             $table->dropForeign('fk_quizzes_user_id');
-========
-            $table->dropColumn(['is_ai_generated']);
->>>>>>>> origin/huydev:be_quizflex/database/migrations/2026_05_31_060842_add_columns_to_quizzes_table.php
+            $table->dropColumn('is_ai_generated');
         });
     }
 };
--- be_quizflex/database/migrations/2026_06_13_173224_modify_provider_in_payments_table.php
+++ be_quizflex/database/migrations/2026_06_13_173224_modify_provider_in_payments_table.php
@@ -9,25 +9,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-<<<<<<<< HEAD:be_quizflex/database/migrations/2026_06_13_173224_modify_provider_in_payments_table.php
             $table->string('provider', 50)->change();
-========
-            $table->foreign(['user_id'], 'fk_payments_user_id')->references(['id'])->on('users')->onUpdate('cascade')->onDelete('cascade');
->>>>>>>> origin/huydev:be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_payments_table.php
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-<<<<<<<< HEAD:be_quizflex/database/migrations/2026_06_13_173224_modify_provider_in_payments_table.php
             $table->enum('provider', ['momo', 'vnpay'])->change();
-========
-            $table->dropForeign('fk_payments_user_id');
->>>>>>>> origin/huydev:be_quizflex/database/migrations/2026_05_18_165815_add_foreign_keys_to_payments_table.php
         });
     }
 };
